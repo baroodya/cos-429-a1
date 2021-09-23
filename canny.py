a1eb5e50fedbae072fceb53d2a2af57ed095270a
Alex Baroody <abaroody@princeton.edu>
2021-09-23 15:05:15 -0400

    Change to Katie's version of canny.py

--- canny.py
+++ canny.py
@@ -1,12 +1,12 @@
 import cv2
-from skimage import io
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def filteredGradient(im, sigma):
     # Computes the smoothed horizontal and vertical gradient images for a given
     # input image and standard deviation. The convolution operation should use
-    # 0 padding.
+    # the default border handling provided by cv2.
     #
     # im: 2D float32 array with shape (height, width). The input image.
     # sigma: double. The standard deviation of the gaussian blur kernel.
@@ -17,63 +17,70 @@ def filteredGradient(im, sigma):
     # Fy: 2D double array with shape (height, width). The vertical
     #     gradients.
 
-    w = int(np.rint(6 * sigma))
-    if w % 2 == 0:
-        w += 1
+    # image in grayscale floating point
+    # img = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY) / 255.0
+    img = im
 
-    h = w
+    def gaussDerivKernel(sigma, isPartialX):
+        # Calculates separated filters of derivatives of 2D Gaussian filter
 
-    # NUM_DATAPOINTS = 1000
+        # make filter odd-sized
+        filter_w = int(np.rint(6 * sigma))
+        if filter_w % 2 == 0:
+            filter_w += 1
 
-    # def two_D_gaussian(squares, sigma):
-    #     return (1 / (2 * np.pi * (sigma ** 2))) * np.e ** (
-    #         -(squares) / (2 * (sigma ** 2))
-    #     )
+        X = np.zeros((filter_w, 1))
+        Y = np.zeros((1, filter_w))
 
-    # squares = np.zeros((NUM_DATAPOINTS, NUM_DATAPOINTS))
-    # for row in range(NUM_DATAPOINTS):
-    #     for col in range(NUM_DATAPOINTS):
-    #         squares[row][col] = ((row - NUM_DATAPOINTS / 2) / NUM_DATAPOINTS) ** 2 + (
-    #             (col - NUM_DATAPOINTS / 2) / NUM_DATAPOINTS
-    #         ) ** 2
-    # io.imshow(two_D_gaussian(squares, sigma))
-    # io.show()
-    # Fx = []
-    # Fy = []
+        # partial derivs of 2D Gaussian split into 1D
+        def xGaussPartialX(x, sigma):
+            frac = -x / (np.sqrt(2 * np.pi) * sigma ** 3)
+            exp = np.exp(-(x ** 2) / (2 * sigma ** 2))
+            return frac * exp
 
-    def dX_gaussian(sigma):
+        def yGaussPartialX(y, sigma):
+            frac = 1 / (np.sqrt(2 * np.pi) * sigma)
+            exp = np.exp(-(y ** 2) / (2 * sigma ** 2))
+            return frac * exp
 
-        filter = np.zeros((h, w))
+        def xGaussPartialY(x, sigma):
+            frac = 1 / (np.sqrt(2 * np.pi) * sigma)
+            exp = np.exp(-(x ** 2) / (2 * sigma ** 2))
+            return frac * exp
 
-        for row in range(h):
-            row_val = float(row - (h / 2))
-            for col in range(w):
-                col_val = float(col - (w / 2))
+        def yGaussPartialY(y, sigma):
+            frac = -y / (np.sqrt(2 * np.pi) * sigma ** 3)
+            exp = np.exp(-(y ** 2) / (2 * sigma ** 2))
+            return frac * exp
 
-                frac = -(row_val / (2 * np.pi * sigma ** 4))
-                exp = np.e ** (-(row_val ** 2 + col_val ** 2) / (2 * sigma ** 2))
+        # load 1D separated filters
+        offset = (filter_w - 1) / 2
+        for i in range(filter_w):
+            val = i - offset
+            if isPartialX:
+                X[i][0] = xGaussPartialX(val, sigma)
+                Y[0][i] = yGaussPartialX(val, sigma)
+            else:
+                X[i][0] = xGaussPartialY(val, sigma)
+                Y[0][i] = yGaussPartialY(val, sigma)
 
-                filter[row][col] = frac * exp
-        return filter
+        return X, Y
 
-    def dY_gaussian(sigma):
-        filter = np.zeros((h, w))
+    # find separable derivatives of 2D Gaussians
+    X_x, Y_x = gaussDerivKernel(sigma, True)  # w.r.t. x
+    X_y, Y_y = gaussDerivKernel(sigma, False)  # w.r.t. y
 
-        for row in range(h):
-            for col in range(w):
-                row_val = float(row - (h / 2))
-                col_val = float(col - (w / 2))
-                frac = -(col_val / (2 * np.pi * sigma ** 4))
-                exp = np.e ** (-(row_val ** 2 + col_val ** 2) / (2 * sigma ** 2))
+    def convolve2DGauss(img, X, Y):
+        # Given horizontal and vertical separable filters, computes convoluted
+        #       image with X then Y (2D Gaussian filter)
 
-                filter[row][col] = frac * exp
-        return filter
+        convolve1 = cv2.filter2D(src=img, ddepth=-1, kernel=X)
+        imgConvolved = cv2.filter2D(src=convolve1, ddepth=-1, kernel=Y)
+        return imgConvolved
 
-    x_der_filter = dX_gaussian(sigma)
-    y_der_filter = dY_gaussian(sigma)
-
-    Fx = cv2.filter2D(im, -1, x_der_filter, 0)
-    Fy = cv2.filter2D(im, -1, y_der_filter, 0)
+    # convolve images with separated Gaussians
+    Fx = convolve2DGauss(img, X_x, Y_x)
+    Fy = convolve2DGauss(img, X_y, Y_y)
 
     return Fx, Fy
 
@@ -91,16 +98,18 @@ def edgeStrengthAndOrientation(Fx, Fy):
     # D: 2D double array with shape (height, width). The edge orientation
     #        image.
 
+    # edge strength F (magnitude of gradient)
     F = np.sqrt(Fx ** 2 + Fy ** 2)
-    assert F.shape == Fx.shape == Fy.shape
 
+    # edge orientation D (D = arctan(Fy/Fx))
     D = np.arctan(Fy / Fx)
-    assert D.shape == Fx.shape == Fy.shape
 
+    # fix orientation to be between 0 and pi
     for row in range(D.shape[0]):
         for col in range(D.shape[1]):
-            if D[row][col] > 0:
+            if D[row][col] < 0:
                 D[row][col] += np.pi
+
     return F, D
 
 
@@ -116,68 +125,67 @@ def suppression(F, D):
     # I: 2D double array with shape (height, width). The output thinned
     #        edge image.
 
-    I = np.zeros(F.shape)
+    height, width = F.shape[0], F.shape[1]
 
-    # Set the intensity of a pixels neighbors based on the direction of the gradient
-    def set_intensity(I, row, col, dir):
-        max_rows, max_cols = F.shape
+    I = np.zeros((height, width))
+    Dstar = np.zeros((height, width))
 
-        # Edge going right/left
-        if dir == 0:
-            if row < max_rows - 1 and F[row + 1][col] > F[row][col]:
+    def setStrength(I, row, col, dirxn):
+        # Finds if a current pixel is smaller than at least one of its neighbors in F
+        # along given dirxn
+
+        nRow, nCol = F.shape[0], F.shape[1]
+        currVal = F[row][col]
+
+        if dirxn == 0:  # edge left and right
+            if row != 0 and currVal < F[row - 1][col]:
                 I[row][col] = 0
                 return
-            if row > 0 and F[row - 1][col] > F[row][col]:
-                I[row][col] = 0
-                return
-            I[row][col] = F[row][col]
-        # Edge going up right/down left
-        elif dir == (np.pi / 4):
-            if (
-                col < max_cols - 1
-                and row < max_rows - 1
-                and F[row + 1][col + 1] > F[row][col]
-            ):
-                I[row][col] = 0
-                return
-            if row > 0 and col > 0 and F[row - 1][col - 1] > F[row][col]:
-                I[row][col] = 0
-                return
-            I[row][col] = F[row][col]
-        # Edge going up/down
-        elif dir == (np.pi / 2):
-            if col < max_cols - 1 and F[row][col + 1] > F[row][col]:
-                I[row][col] = 0
-                return
-            if row > 0 and F[row][col - 1] > F[row][col]:
-                I[row][col] = 0
-                return
-            I[row][col] = F[row][col]
-        # Edge going up left/down right
-        else:
-            if col < max_cols - 1 and row > 0 and F[row - 1][col + 1] > F[row][col]:
-                I[row][col] = 0
-                return
-            if row < max_rows - 1 and col > 0 and F[row + 1][col - 1] > F[row][col]:
+            if row != nCol - 1 and currVal < F[row + 1][col]:
                 I[row][col] = 0
                 return
             I[row][col] = F[row][col]
 
-    best_dirs = np.zeros(F.shape)
-    for row in range(F.shape[0]):
-        for col in range(F.shape[1]):
-            # change direction to be one of [0, pi/4, pi/2, 3pi/4]
-            dir = D[row][col]
-            if dir >= (np.pi / 8) and dir < (3 * np.pi / 8):
-                best_dirs[row][col] = np.pi / 4
-            elif dir >= (3 * np.pi / 8) and dir < (5 * np.pi / 8):
-                best_dirs[row][col] = np.pi / 2
-            elif dir >= (5 * np.pi / 8) and dir < (7 * np.pi / 8):
-                best_dirs[row][col] = 3 * np.pi / 4
+        elif dirxn == np.pi / 4:  # edge diag top right and diag bottom left
+            if row != nRow - 1 and col != nCol - 1 and currVal < F[row + 1][col + 1]:
+                I[row][col] = 0
+                return
+            if row != 0 and col != 0 and currVal < F[row - 1][col - 1]:
+                I[row][col] = 0
+                return
+            I[row][col] = F[row][col]
+
+        elif dirxn == np.pi / 2:  # above and below
+            if col != 0 and currVal < F[row][col - 1]:
+                I[row][col] = 0
+                return
+            if col != nCol - 1 and currVal < F[row][col + 1]:
+                I[row][col] = 0
+                return
+            I[row][col] = F[row][col]
+
+        else:  # diag top left and diag bottom right
+            if row != 0 and col != nCol - 1 and currVal < F[row - 1][col + 1]:
+                I[row][col] = 0
+                return
+            if row != nRow - 1 and col != 0 and currVal < F[row + 1][col - 1]:
+                I[row][col] = 0
+                return
+            I[row][col] = F[row][col]
+
+    # Update D* based on arctan
+    for i in range(height):
+        for j in range(width):
+            if D[i][j] < (np.pi / 8) or D[i][j] > (7 * np.pi / 8):
+                Dstar[i][j] = 0
+            elif D[i][j] < (3 * np.pi / 8):
+                Dstar[i][j] = np.pi / 4
+            elif D[i][j] < (5 * np.pi / 8):
+                Dstar[i][j] = np.pi / 2
             else:
-                best_dirs[row][col] = 0
+                Dstar[i][j] = 3 * np.pi / 4
 
-            set_intensity(I, row, col, best_dirs[row][col])
+            setStrength(I, i, j, Dstar[i][j])
 
     return I
 
@@ -196,68 +204,79 @@ def hysteresisThresholding(I, D, tL, tH):
     # edgeMap: 2D binary array with shape (height, width). Output edge map,
     #          where edges are 1 and other pixels are 0.
 
-    # Check to see if the neighbors of a pixel are greater than the low threshold
-    def check_neighbors(row, col, dir, edgeMap, markedMap):
-        max_rows, max_cols = I.shape
+    height = I.shape[0]
+    width = I.shape[1]
 
-        markedMap[row][col] = True
+    # Normalize I
+    maxI = np.amax(I)
+    I = I / maxI
 
-        # Edge going right/left
-        if dir == 0:
-            if row < max_rows - 1:
-                markedMap[row + 1][col] = True
-                if I[row + 1][col] > tL:
-                    edgeMap[row + 1][col] = True
-            if row > 0:
-                markedMap[row - 1][col] = True
-                if I[row - 1][col] > tL:
-                    edgeMap[row - 1][col] = True
-        # Edge going up left/down right
-        elif dir == (np.pi / 4):
-            if row < max_rows - 1 and col < max_cols - 1:
-                markedMap[row + 1][col + 1] = True
-                if I[row + 1][col + 1] > tL:
-                    edgeMap[row + 1][col + 1] = True
-            if row > 0 and col > 0:
-                markedMap[row - 1][col - 1] = True
-                if I[row - 1][col - 1] > tL:
-                    edgeMap[row - 1][col - 1] = True
-        # Edge going up/down
-        elif dir == (np.pi / 2):
-            if col < max_cols - 1:
-                markedMap[row][col + 1] = True
-                if I[row][col + 1] > tL:
-                    edgeMap[row][col + 1] = True
-            if col > 0:
-                markedMap[row][col - 1] = True
-                if I[row][col - 1] > tL:
-                    edgeMap[row][col - 1] = True
-        # Edge going up right.down left
-        else:
-            if row < max_rows - 1 and col > 0:
-                markedMap[row + 1][col - 1] = True
-                if I[row + 1][col - 1] > tL:
-                    edgeMap[row + 1][col - 1] = True
-            if row > 0 and col < max_cols - 1:
-                markedMap[row - 1][col + 1] = True
-                if I[row - 1][col - 1] > tL:
-                    edgeMap[row - 1][col + 1] = True
+    # 2D boolean array of pixels (x, y) such that I(x, y) > T_h
+    threshH = I > tH
 
-    # Normalize the intensity matrix
-    max = np.max(I)
-    I = I / max
+    visited = np.zeros((height, width), dtype=bool)
+    edgeMap = np.zeros((height, width), dtype=bool)
 
-    # only take values that pass the high threshold
-    edgeMap = I > tH
+    for row in range(height):
+        for col in range(width):
+            # check if hasn't been checked and meets tH threshold
+            if threshH[row][col] and not visited[row][col]:
+                visited[row][col] = True
+                edgeMap[row][col] = True
 
-    # Find the neighbors of the passing pixels that pass the low threshold
-    markedMap = I > np.Infinity
-    for row in range(I.shape[0]):
-        for col in range(I.shape[1]):
-            if edgeMap[row][col] and not markedMap[row][col]:
-                check_neighbors(row, col, D[row][col], edgeMap, markedMap)
+                # start search for light edges
+                searchLightEdge(row, col, I, D, visited, edgeMap, tL)
 
-    return edgeMap.astype(float)
+    edgeMap = edgeMap.astype(np.float)
+
+    return edgeMap
+
+
+def searchLightEdge(row, col, I, D, visited, edgeMap, tL):
+    # check neighbors of row, col for meeting threshold
+
+    nRow = I.shape[0]
+    nCol = I.shape[1]
+
+    # 0: left and right
+    if D[row][col] < (np.pi / 8) or D[row][col] > (7 * np.pi / 8):
+        if col != 0 and not visited[row][col - 1]:
+            updateLightEdge(row, col - 1, I, D, visited, edgeMap, tL)
+
+        if col != nCol - 1 and not visited[row][col + 1]:
+            updateLightEdge(row, col + 1, I, D, visited, edgeMap, tL)
+
+    # pi/4: diag top right and diag bottom left
+    elif D[row][col] < (3 * np.pi / 8):
+        if row != 0 and col != nCol - 1 and not visited[row - 1][col + 1]:
+            updateLightEdge(row - 1, col + 1, I, D, visited, edgeMap, tL)
+
+        if row != nRow - 1 and col != 0 and not visited[row + 1][col - 1]:
+            updateLightEdge(row + 1, col - 1, I, D, visited, edgeMap, tL)
+
+    # pi/2: above and below
+    elif D[row][col] < (5 * np.pi / 8):
+        if row != 0 and not visited[row - 1][col]:
+            updateLightEdge(row - 1, col, I, D, visited, edgeMap, tL)
+
+        if row != nRow - 1 and not visited[row + 1][col]:
+            updateLightEdge(row + 1, col, I, D, visited, edgeMap, tL)
+
+    # 3pi/4: diag top left and diag bottom right
+    else:
+        if row != 0 and col != 0 and not visited[row - 1][col - 1]:
+            updateLightEdge(row - 1, col - 1, I, D, visited, edgeMap, tL)
+
+        if row != nRow - 1 and col != nCol - 1 and not visited[row + 1][col + 1]:
+            updateLightEdge(row + 1, col + 1, I, D, visited, edgeMap, tL)
+
+
+def updateLightEdge(row, col, I, D, visited, edgeMap, tL):
+    # update visited, edgeMap, and initiate another search on neighbors
+    visited[row][col] = True
+    if I[row][col] > tL:
+        edgeMap[row][col] = True
+        searchLightEdge(row, col, I, D, visited, edgeMap, tL)
 
 
 def cannyEdgeDetection(im, sigma, tL, tH):
@@ -281,13 +300,32 @@ def cannyEdgeDetection(im, sigma, tL, tH):
     # edgeMap: 2D binary image of shape (height, width). Output edge map,
     #          where edges are 1 and other pixels are 0.
 
-    # TODO: Implement me!
-    # Normalize the image
-    float_im = im / 255.0
+    # filtered gradient
+    Fx, Fy = filteredGradient(im, sigma)
 
-    Fx, Fy = filteredGradient(float_im, sigma)
+    # plt.imshow(Fx, cmap="gray")
+    # plt.title("Fx")
+    # plt.show()
+
+    # plt.imshow(Fy, cmap="gray")
+    # plt.title("Fy")
+    # plt.show()
+
+    # edge strength and orientation
     F, D = edgeStrengthAndOrientation(Fx, Fy)
+
+    # plt.imshow(F, cmap="gray")
+    # plt.title("F")
+    # plt.show()
+
+    # suppression
     I = suppression(F, D)
+
+    # plt.imshow(I, cmap="gray")
+    # plt.title("I")
+    # plt.show()
+
+    # hysteresisThresholding
     edgeMap = hysteresisThresholding(I, D, tL, tH)
 
     return edgeMap
